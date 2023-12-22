7da62168bda095cb31f5806774fd09be1b465342
Tobiasz Tonn <toneq@tlen.pl>
2023-12-22 16:01:33 +0100

    Dodanie widoku rejestracji oraz samej rejestracji użytkownika

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,14 +5,36 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/registration', name: 'app_registration')]
-    public function index(): Response
+    #[Route(path: '/create', name: 'app_create')]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, PersistenceManagerRegistry $doctrine): Response
     {
-        return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
-        ]);
+        if ($request->isMethod('POST')) {
+            $password = $request->request->get('password');
+            $email = $request->request->get('email');
+
+            $user = new User();
+            $user->setEmail($email);
+            
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $password
+            );
+            $user->setPassword($hashedPassword);
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('post_index');
+        } else {
+            return $this->redirectToRoute('app_register');
+        }
     }
 }
