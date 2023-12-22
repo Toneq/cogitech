9de87043d6d9261c88f14c0ce3bf8869e1622232
Tobiasz Tonn <toneq@tlen.pl>
2023-12-22 16:31:45 +0100

    Dodanie wyświetlenia postów na podstronie /lista oraz pobranie jako api /posts

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PostController extends AbstractController
 {
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    #[Route(path: '/lista', name: 'post_index')]
+    public function index(PersistenceManagerRegistry $doctrine, Security $security): Response
     {
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
-        ]);
+        if ($security->isGranted('ROLE_USER')) {
+            $posts = $doctrine->getRepository(Post::class)->findAll();
+            return $this->render('post/index.html.twig', [
+                'posts' => $posts,
+            ]);
+        } else {
+            return $this->redirectToRoute('app_login');
+        }
+    }
+
+    #[Route(path: '/posts', name: 'api_post_index')]
+    public function apiIndex(PersistenceManagerRegistry $doctrine): JsonResponse
+    {
+        $posts = $doctrine->getRepository(Post::class)->findAll();
+        $data = [];
+
+        foreach ($posts as $post) {
+            $data[] = [
+                'id' => $post->getId(),
+                'title' => $post->getTitle(),
+                'body' => $post->getBody(),
+                'author' => $post->getAuthor(),
+            ];
+        }
+
+        return new JsonResponse($data);
     }
 }
