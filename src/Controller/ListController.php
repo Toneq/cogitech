ede4d1ef563c63303c987f3cd530d2383bba9fe7
Tobiasz Tonn <toneq@tlen.pl>
2024-05-05 21:49:52 +0200

    Ciąg dalszy wyświetlania listy postów oraz autha

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ListController extends AbstractController
 {
-    #[Route('/list', name: 'app_list')]
-    public function index(): Response
+    #[Route('/lista', name: 'app_list')]
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $posts = $entityManager->getRepository(Post::class)->findAll();
+
         return $this->render('list/index.html.twig', [
-            'controller_name' => 'ListController',
+            'posts' => $posts,
         ]);
     }
 }
